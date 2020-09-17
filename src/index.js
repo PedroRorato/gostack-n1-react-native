9bcdf33f91aa17070b8121963f2d2e554fc71a19
Pedro <pedroberorato@gmail.com>
2020-09-17 00:18:27 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+export default function App() {
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <View style={styles.container}>
+                <Text>oi</Text>
+            </View>
+        </>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center'   
+    }
+});
